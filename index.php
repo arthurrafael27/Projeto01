3b8dcfd11a5441b684749324a4b4feb5e5115a65
arthurrafael27 <arthurrafaelaraujomonteiro5@gmail.com>
2024-07-17 21:16:37 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo/all.min.css">
+   <link rel="stylesheet" href="style.css">
+    
+    
+    <title>Projeto01</title>
+</head>
+<body>
+
+
+<header>
+    <div class="center"></div>
+    <div class="logo left">Logomarca</div>
+    <nav class="desktop right">
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Sobre</a></li>
+            <li><a href="">Serviços</a></li>
+            <li><a href="">Contato</a></li>
+        </ul>
+    </nav>
+    <nav class="mobile right">
+        <div class="botao-menu-mobile">
+        <i class="fa-solid fa-bars"></i>
+
+        </div>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Sobre</a></li>
+            <li><a href="">Serviços</a></li>
+            <li><a href="">Contato</a></li>
+        </ul>
+    </nav>
+<div class="clear"></div>
+    </div><!--center-->
+
+</header>
+
+<section class="banner-principal">
+
+<div class="center">
+
+<form action="">
+    <h2>Qual seu Email?</h2>
+    <input type="email" name="email" require>
+    <input type="submit" name="acao" value="Cadastrar">
+</form>
+
+</div><!--center-->
+
+</section>
+
+<div class="center">
+<section class="descricao-autor">
+
+<h2>Arthur Monteiro</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi quaerat, sit rerum doloremque sed aperiam neque quidem ea voluptate laboriosam nisi beatae libero deleniti unde distinctio, doloribus fugit aut dolorem?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi quaerat, sit rerum doloremque sed aperiam neque quidem ea voluptate laboriosam nisi beatae libero deleniti unde distinctio, doloribus fugit aut dolorem?</p>
+
+
+</div>
+<div class="w50 left">
+
+<div class="clear"></div>
+</div><!--center-->
+
+</section>
+
+<section class="especialidades">
+
+
+<div class="center">
+<h2 class="title">Especialidades</h2>
+    <div class="w33 left box-especialidade">
+<h3><i class="fa-brands fa-html5"></i></h3>
+<h4>CSS3</h4>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis libero quis maxime nam? Sequi dolore consequuntur, iste quas dolor culpa, delectus doloribus, dolores sunt tempore dignissimos illum ex perspiciatis veritatis.</p>
+
+    </div><!--box-especialidade-->
+
+    <div class="w33 left box-especialidade">
+<h3><i class="fa-brands fa-css3-alt"></i></i></h3>
+<h4>HTML5</h4>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis libero quis maxime nam? Sequi dolore consequuntur, iste quas dolor culpa, delectus doloribus, dolores sunt tempore dignissimos illum ex perspiciatis veritatis.</p>
+
+</div><!--box-especialidade-->
+<div class="w33 left box-especialidade">
+
+
+
+<h3><i class="fa-brands fa-js"></i></h3>
+<h4>JavaScript</h4>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis libero quis maxime nam? Sequi dolore consequuntur, iste quas dolor culpa, delectus doloribus, dolores sunt tempore dignissimos illum ex perspiciatis veritatis.</p>
+
+</div><!--box-especialidade-->
+<div class="clear"></div>
+
+</div>
+
+</section>
+
+<section class="extras">
+
+<div class="center">
+<h2 class="title">Depoimentos</h2>
+<div class="depoimento-single">
+<p class="depoimento-descricao">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo architecto eum dolore quaerat voluptas, repellendus soluta fugit quas. Sed, debitis suscipit officiis quas excepturi optio obcaecati aliquid fuga vel neque?</p>
+<p class="nome-autor">Jorge</p>
+
+<div class="depoimento-single">
+<p class="depoimento-descricao">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo architecto eum dolore quaerat voluptas, repellendus soluta fugit quas. Sed, debitis suscipit officiis quas excepturi optio obcaecati aliquid fuga vel neque?</p>
+<p class="nome-autor">Jorge</p>
+
+<div class="depoimento-single">
+<p class="depoimento-descricao">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo architecto eum dolore quaerat voluptas, repellendus soluta fugit quas. Sed, debitis suscipit officiis quas excepturi optio obcaecati aliquid fuga vel neque?</p>
+<p class="nome-autor">Jorge</p>
+</div><!--depoimento-single-->
+
+</div><!--w50-->
+
+
+</div><!--center-->
+
+
+</section><!--extras-->
+
+<section class="especialidades">
+
+
+<div class="center">
+<h2 class="title">Meios de comunicação</h2>
+    <div class="w33 left box-servicos">
+<h3><i class="fa-brands fa-instagram"></i></h3>
+<h4>Instagram</h4>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis libero quis maxime nam? Sequi dolore consequuntur, iste quas dolor culpa, delectus doloribus, dolores sunt tempore dignissimos illum ex perspiciatis veritatis.</p>
+
+    </div><!--box-servicos-->
+
+    <div class="w33 left box-servicos">
+<h3><i class="fa-brands fa-github"></i></h3>
+<h4>Github</h4>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis libero quis maxime nam? Sequi dolore consequuntur, iste quas dolor culpa, delectus doloribus, dolores sunt tempore dignissimos illum ex perspiciatis veritatis.</p>
+
+</div><!--box-box-servicos-->
+<div class="w33 left box-servicos">
+
+
+
+<h3><i class="fa-brands fa-linkedin"></i></h3>
+<h4>Linkedin</h4>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis libero quis maxime nam? Sequi dolore consequuntur, iste quas dolor culpa, delectus doloribus, dolores sunt tempore dignissimos illum ex perspiciatis veritatis.</p>
+
+</div><!--box-servicos-->
+<div class="clear"></div>
+
+</div>
+
+</section>
+
+<footer>
+    <p>Site do Arthur</p>
+</footer>
+
+
+
+    
+</body>
+</html>
